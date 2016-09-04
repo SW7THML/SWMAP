6fbc21c422eea9e4a12b76169ee8b2293ca980c3
Taekmin Kim <tantara.tm@gmail.com>
2016-09-04 17:17:30 +0900

    change paperclip to carrierwave

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,8 +13,7 @@
 class Profile < ActiveRecord::Base
   has_many :comments
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  mount_uploader :avatar, AvatarUploader
 
 	def self.search(text)
     query = "%#{text}%"
--- db/migrate/20160904071717_add_avatar_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarToProfile < ActiveRecord::Migration[5.0]
-  def up
-    add_attachment :profiles, :avatar
-  end
-
-  def down
-    remove_attachment :profiles, :avatar
-  end
-end
