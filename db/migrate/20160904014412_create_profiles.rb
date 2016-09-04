214eeb087fabc212593555adac697cfb9cb83199
김슬아 <SeulA@SeulA.local>
2016-09-04 10:49:14 +0900

    Add the profile model

---
+++ db/migrate/20160904014412_create_profiles.rb
@@ -0,0 +1,10 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+    	t.string :name
+    	t.string :email
+    	t.string :phone_number
+    	t.timestamps 
+    end
+  end
+end
