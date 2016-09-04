11b175147aff473df08831f139f2ca543457113f
김슬아 <SeulA@SeulA.local>
2016-09-04 16:14:04 +0900

    Comment2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,22 @@ class CommentsController < ApplicationController
 		#redirect_to root_path
 	end
 
+	def destroy
+		@profile = Profile.find(params[:profile_id])
+		@comment = @profile.comments.find(params[:id])
+		@comment.destroy
+		redirect_to profile_path(@profile)
+	end
+
+	def show
+  		@profile = Profile.find(params[:profile_id])
+		render :layout => false
+  #respond_to do |format|
+  #    format.html
+  #    format.json { render "_modal.html.erb", :layout => false }
+  #  end
+  end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
